53c7fccdfe227cc3827356a86c861fc039d58396
BearJoeWYL <wylsniper@163.com>
2018-02-28 18:15:09 +0800

    master 2018-02-28 18:13:43 select area events added

--- app/lib/alg/select/select.js
+++ app/lib/alg/select/select.js
@@ -1,41 +1,77 @@
 var select = {
 	defaultSelectClassName:{
 		0:"selectable",
-		1:"un-selected",
-		2:"selected",
+		1:"selected",
 	},
 	applyAction: {
 		clickSelect:function(element){
 			if(element){
-			
+				element.addEventListener("click", function(event){
+					let ele = this
+					if(ele.classList.contains(select.defaultSelectClassName[0])){
+                        element.classList.toggle(select.defaultSelectClassName[1])
+					}
+				})
 			}
 		},
 		dragSelect:function(element){
-			if(element && element.offsetParent){
-				let offsetParent = element.offsetParent
-				offsetParent.addEventListener("dragstart", function(event){
-					console.log(this)
-					console.log(event);
-					console.log("drag start");
+			if(element){
+				let ele = element
+				ele.addEventListener("mousedown", function(event){
+				    event.preventDefault()
+					let ele = this
+					let selectables = ele.querySelectorAll(select.defaultSelectClassName[0])
+					for(let i = 0; i < ele.length; i++){
+                        selectables[i].classList.remove(select.defaultSelectClassName[1])
+					}
+					// console.log(`____________________
+// button:${event.button};
+// buttons:${event.buttons};
+// client:${event.clientX},${event.clientY};
+// layer:${event.layerX},${event.layerY};
+// movement:${event.movementX},${event.movementY};
+// page:${event.pageX},${event.pageY};
+// screen:${event.screenX}, ${event.screenY}; `);
+
 				});
-				offsetParent.addEventListener("dragover", function(event){
-					// console.log(event);
-					// console.log("drag overing");
+				ele.addEventListener("mousemove", function(event){
+                    event.preventDefault()
+					let ele = this
+//                     console.log(`____________________
+// button:${event.button};
+// buttons:${event.buttons};
+// client:${event.clientX},${event.clientY};
+// layer:${event.layerX},${event.layerY};
+// movement:${event.movementX},${event.movementY};
+// page:${event.pageX},${event.pageY};
+// screen:${event.screenX}, ${event.screenY}; `);
 				});
-				offsetParent.addEventListener("dragend", function(event){
-					console.log(this)
-					console.log(event);
-					console.log("drag end");
+				ele.addEventListener("mouseout", function(event){
+					event.preventDefault()
+	                console.log(`mouseout--button:${event.button};buttons:${event.buttons};client:${event.clientX},${event.clientY};layer:${event.layerX},${event.layerY};movement:${event.movementX},${event.movementY};page:${event.pageX},${event.pageY};screen:${event.screenX}, ${event.screenY}; `);
+				});
+				ele.addEventListener("mouseenter", function(event){
+					event.preventDefault()
+					console.log(`mouseenter--button:${event.button};buttons:${event.buttons};client:${event.clientX},${event.clientY};layer:${event.layerX},${event.layerY};movement:${event.movementX},${event.movementY};page:${event.pageX},${event.pageY};screen:${event.screenX}, ${event.screenY}; `);
+				});
+				ele.addEventListener("dragstart", function(event){
+					console.log("dragstart")
+				});
+				ele.addEventListener("dragover", function(event){
+					console.log("dragovering")
+				});
+				ele.addEventListener("dragend", function(event){
+					console.log("dragend")
 				});
 			}
 		},
 	},
 	selectSeal:{
 		select:function(element){
-		
+			element.classList.add(select.defaultSelectClassName[1])
 		},
 		deselect:function(element){
-		
+            element.classList.remove(select.defaultSelectClassName[1])
 		},
 	},
 }
